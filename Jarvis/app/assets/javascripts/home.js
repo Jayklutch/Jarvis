3868d4ef4d1e6b091c60f32a5a52e072280b1d2f
Jayklutch <klutchdacar@outlook.com>
2015-05-20 16:24:29 +1200

    Keyboard input working

--- Jarvis/app/assets/javascripts/home.js
+++ Jarvis/app/assets/javascripts/home.js
@@ -1,9 +1,3 @@
-
-$("#L").on('click', function(e){
-  alert("The paragraph was clicked.");
-  $("#r1").css("color", "red");
-});
-
 $(document).keydown(function(e) {
   console.log(e.which);
       $("#"+e.which).css("color", "red");
